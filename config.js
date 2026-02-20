3fba9dd04dd05cd8664de94275eaabef6db7bafd
Dark-Nero-V6 <nerodark445@gmail.com>
2026-02-20 23:25:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,24 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "JVEFhRLR#br8dtcSUXUzognByf2mNhArd2w_SUHNBC8OeqpfdRU0",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Dark-Nero-V6/Dark-Nero-V7/blob/main/Images/FekrFcb2R2G72ZxKDAF2dg.webp?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hellow...👋*
-
-I am alive now....🔥
-
-🤖 RED DRAGON V 1.0.0 🐉
-━━━━━━━━━━━━━━━━━━━
-✅ Status: ONLINE
-⚡ Speed: Super Fast
-🧠 Mode: Hacker Activated
-👑 Owner: Rashmika
-
-🔥 System running perfectly…
-💬 Send commands and enjoy!
-
-“Power isn’t given… it’s taken.” 😈
-
-> Powered by Rashmika Tec...
-━━━━━━━━━━━━━━━━━━━",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hellow...👋*\n\nI am alive now....🔥\n\n*🤖 RED DRAGON V 1.0.0 🐉*\n━━━━━━━━━━━━━━━━━━━\n✅ Status: ONLINE\n⚡ Speed: Super Fast\n🧠 Mode: Hacker Activated\n👑 Owner: Rashmika\n\n🔥 System running perfectly…\n💬 Send commands and enjoy!\n\n“Power isn’t given… it’s taken.” 😈\n\n> Powered by Rashmika Tec...\n━━━━━━━━━━━━━━━━━━━",
 BOT_OWNER: '94764771497',  // Replace with the owner's phone number
 
 
