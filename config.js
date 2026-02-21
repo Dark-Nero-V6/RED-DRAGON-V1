71c76b3c1fabf8b7dcd218af2412609232d6574a
Dark-Nero-V6 <nerodark445@gmail.com>
2026-02-21 23:07:58 +0530

    🔥RED-DRAGON🔥

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "JVEFhRLR#br8dtcSUXUzognByf2mNhArd2w_SUHNBC8OeqpfdRU0",
-GEMINI_API_KEY: process.env.GEMINI_API_KEY || 'AIzaSyCuiBh4Ru0bp3BkK49XREWq_olnKWm7Eo0',
+GEMINI_API_KEY: process.env.GEMINI_API_KEY || 'AIzaSyCgt39H5jkSCtViuf8xidFyv-AGV8dL4i8',
 MOVIE_API_KEY: process.env.MOVIE_API_KEY || 'SiR9-_P3UaQ4NUvznbbMuKCew94FlLc5mhwfQMmm7XI',
 ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Dark-Nero-V6/Dark-Nero-V7/blob/main/Images/FekrFcb2R2G72ZxKDAF2dg.webp?raw=true",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hellow...👋*\n\nI am alive now....🔥\n\n*🤖 RED DRAGON V 1.0.0 🐉*\n━━━━━━━━━━━━━━━━━━━\n✅ Status: ONLINE\n⚡ Speed: Super Fast\n🧠 Mode: Hacker Activated\n👑 Owner: Rashmika\n\n🔥 System running perfectly…\n💬 Send commands and enjoy!\n\n“Power isn’t given… it’s taken.” 😈\n\n> Powered by Rashmika Tec...\n━━━━━━━━━━━━━━━━━━━",
