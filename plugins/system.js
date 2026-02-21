3701b7804aca3e29d6b24f7d0b318e3e293cb6a0
Dark-Nero-V6 <nerodark445@gmail.com>
2026-02-21 14:44:08 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -17,7 +17,7 @@ async (danuwa, mek, m, {
     groupAdmins, isBotAdmins, isAdmins, reply
 }) => {
     try{
-    let status = '╭━━━〔 🧬 RED-DRAGON🔥 〕━━━⬣\n┃ 👑 Owner : Rashmika\n┃ ⚡ Speed  : ${ping} ms\n┃ 🕒 Uptime : ${runtime(process.uptime())}\n┃ ⚜ Mode   : Public\n╰━━━━━━━━━━━━━━━━⬣\n\n\n> Powered By Rashmika Tec...'
+    let status = '*Uptime:* ${runtime(process.uptime())}\n*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${math.round(require('os').totalmem / 1024 / 1024)}MB\n*Host name:* ${os.hostname()}\n*Owner:* Sasidu Rashmika\n\n> Powered By Rashmika Tec...'
     return reply(`${status}`)
 
     }catch(e){
