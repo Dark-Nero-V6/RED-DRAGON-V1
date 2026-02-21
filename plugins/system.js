9239956f36b5d2d9b594d55316a1604a54d38099
Dark-Nero-V6 <nerodark445@gmail.com>
2026-02-21 14:46:21 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -17,7 +17,7 @@ async (danuwa, mek, m, {
     groupAdmins, isBotAdmins, isAdmins, reply
 }) => {
     try{
-    let status = '*Uptime:* ${runtime(process.uptime())}\n*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${math.round(require('os').totalmem / 1024 / 1024)}MB\n*Host name:* ${os.hostname()}\n*Owner:* Sasidu Rashmika\n\n> Powered By Rashmika Tec...'
+    let status = '*Uptime:* ${runtime(process.uptime())}\n*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${math.round(require(`os`).totalmem / 1024 / 1024)}MB\n*Host name:* ${os.hostname()}\n*Owner:* Sasidu Rashmika\n\n> Powered By Rashmika Tec...'
     return reply(`${status}`)
 
     }catch(e){
