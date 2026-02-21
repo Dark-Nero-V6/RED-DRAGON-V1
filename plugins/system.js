0a8540be43d5365092af3f4c20ab6f93494ba57a
Dark-Nero-V6 <nerodark445@gmail.com>
2026-02-21 12:49:29 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,31 @@
+const { cmd, commands } = require('../command');
+const config = require('../config');
+const os = require("os")
+cmd({
+    pattern: "system",
+    react: "🧬",
+    desc: "Check bot system",
+    category: "main",
+    filename: __filename
+},
+async (danuwa, mek, m, {
+    from, quoted, body, isCmd, command, args, q, isGroup,
+    sender, senderNumber, botNumber2, botNumber, pushname,
+    isMe, isOwner, groupMetadata, groupName, participants,
+    groupAdmins, isBotAdmins, isAdmins, reply
+}) => {
+    try{
+let status = '*Uptime:* ${runtime(process.uptime())}
+  *Rum usage:* ${(process.memoryUsage().heapUsed / 1024 /1024).toFixed(2)}MB / ${math.round(require('os').totalmem / 1024 / 1024)MB
+  *Host name:* ${os.hostname()}
+  *Owner:* Sasidu Rashmika'
+                                                                                 
+   return reply('${status}')
+                                                                                 
+                                                                                 
+    }catch(e)}
+    console.log(e)
+    reply('${e}')
+
+    }
+    })
